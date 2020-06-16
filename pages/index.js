5d2732890984ce96cc8b38d9122165ac91420705
adam baranes <adambaranes@gmail.com>
2020-06-16 17:09:30 +0300

    2 graph works

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,211 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+// import Head from 'next/head'
+// import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from '../store/actionCreators';
 import Button from '@material-ui/core/Button';
-import * as actionTypes from '../store/actions';
-
+import FilesTimePieChart from '../components/pie-chart/files-time-pie-chart';
+import LineChart from '../components/line-chart/line-chart'
 const Home = (props) => {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
+      <div className={utilStyles.gridContainer}>
+        <div className={utilStyles.item1}>
+          התראות
+        </div>
+        <div className={utilStyles.item2}>
+          <LineChart data={lineData}></LineChart>
+        </div>
+        <div className={utilStyles.item3}>
+          <FilesTimePieChart data={pieData}></FilesTimePieChart>
+        </div>  
+        <div className={utilStyles.item4}>
+          הודsadasdעות
+        </div>
+        <div className={utilStyles.item5}>
+        טבלה
+        </div>
+        <div className={utilStyles.item6}>
+        שיטת התקשרות
+        </div>
+        <div className={utilStyles.item7}>
+        תגיות
+        </div>
+      {
+        //store section
+        /* <section className={utilStyles.headingMd}>
         <p>software developer at Kiloma!</p>
         <Button onClick={props.onPostAdded} variant="contained" color="primary">
         ADD
-      </Button>
+        </Button>
         <div>number of posts: {props.numOfPosts}</div>
         <Button onClick={props.onPostRemoved} variant="contained" color="secondary">
         REMOVE
-      </Button>
-      <div></div>
-      <Button onClick={props.onInitPosts} variant="contained" color="default">
+        </Button>
+        <div></div>
+        <Button onClick={props.onInitPosts} variant="contained" color="default">
         INITIALIZE POSTS
-      </Button>
+        </Button>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        (This is a sample website - you’ll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-      </section>
-    </Layout>
-  ) 
+      </section> */}
+      </div>
+  )
 }
 
 
 const mapStateToProps = state => {
-  return{
+  return {
     numOfPosts: state.numOfPosts
   }
 }
 
-const mapDispatchToProps = dispatch =>{
-  return{
-    onPostAdded: ()=> dispatch(actions.add()),
-    onPostRemoved: ()=> dispatch(actions.remove()),
-    onInitPosts: ()=> dispatch(actions.initPosts())
+const mapDispatchToProps = dispatch => {
+  return {
+    onPostAdded: () => dispatch(actions.add()),
+    onPostRemoved: () => dispatch(actions.remove()),
+    onInitPosts: () => dispatch(actions.initPosts())
   }
 }
 
-// export async function getStaticProps() {
-//   const allPostsData = getSortedPostsData()
-//   return {
-//     props: {
-//       allPostsData
-//     }
-//   }
-// }
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
-export default connect(mapStateToProps,mapDispatchToProps)(Home);
+const pieData = [
+  {
+    "id": "15",
+    "label": "תקין",
+    "value": 15,
+    "color": "rgb(255, 0, 0))"
+  },
+  {
+    "id": "8",
+    "label": "קרוב לחריגה",
+    "value": 8,
+    "color": "rgb(0, 204, 0)"
+  },
+  {
+    "id": "2",
+    "label": "חורג",
+    "value": 2,
+    "color": "rgb(255, 0, 0)"
+  },
+]
 
 
-
-    // <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-    //   {/* <h2 className={utilStyles.headingLg}>Blog</h2> */}
-    //   {/* <ul className={utilStyles.list}> */}
-    //     {allPostsData.map(({ id, date, title }) => (
-    //       <li className={utilStyles.listItem} key={id}>
-    //         <Link href="/posts/[id]" as={`/posts/${id}`}>
-    //           <a>{title}</a>
-    //         </Link>
-    //         <br />
-    //         <small className={utilStyles.lightText}>
-    //           <Date dateString={date} />
-    //         </small>
-    //       </li>
-    //     ))}
-    //   {/* </ul> */}
-    // </section>
+const lineData = [
+  {
+    "id": "חורג",
+    "color":"red",
+    "data": [
+      {
+        "x": 0,
+        "y": 1.5
+      },
+      {
+        "x": 5,
+        "y": 3
+      },
+      {
+        "x": 10,
+        "y": 2.5
+      },
+      {
+        "x": 15,
+        "y": 2
+      },
+      {
+        "x": 20,
+        "y": 1
+      },
+      {
+        "x": 25,
+        "y": 1
+      },
+      {
+        "x": 30,
+        "y": 0.5
+      },
+      {
+        "x": 35,
+        "y": 0
+      }
+    ]
+  },
+  {
+    "id": "כמעט חורג",
+    "color":"orange",
+    "data": [
+      {
+        "x": 0,
+        "y": 3
+      },
+      {
+        "x": 5,
+        "y": 2
+      },
+      {
+        "x": 10,
+        "y": 1
+      },
+      {
+        "x": 15,
+        "y": 0.5
+      },
+      {
+        "x": 20,
+        "y": 2
+      },
+      {
+        "x": 25,
+        "y": 3
+      },
+      {
+        "x": 30,
+        "y": 3
+      },
+      {
+        "x": 35,
+        "y": 3.5
+      }
+    ]
+  },
+  {
+    "id": "תקין",
+    "color":"green",
+    "data": [
+      {
+        "x": 0,
+        "y": 1
+      },
+      {
+        "x": 5,
+        "y": 0.5
+      },
+      {
+        "x": 10,
+        "y": 1
+      },
+      {
+        "x": 15,
+        "y": 2
+      },
+      {
+        "x": 20,
+        "y": 2
+      },
+      {
+        "x": 25,
+        "y": 3
+      },
+      {
+        "x": 30,
+        "y": 3.5
+      },
+      {
+        "x": 35,
+        "y": 4
+      }
+    ]
+  }
+]

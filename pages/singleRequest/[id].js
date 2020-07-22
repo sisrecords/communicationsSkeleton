0a19e5e4406b08690004947fd210619bf46a9358
adam baranes <adambaranes@gmail.com>
2020-07-22 16:07:40 +0300

    half form ready , mooving to css grid instead.

--- pages/singleRequest/[id].js
+++ pages/singleRequest/[id].js
@@ -7,32 +7,7 @@ import * as actions from "../../store/actionCreators";
 
 export function SingleRequest(props) {
   const router = useRouter();
-  //console.log(router.query.id);
-  const [dataObject, setDataObject] = useState(
-    initialState.mainTableData[router.query.id]
-  );
-  // const pageObject = () => {
-  //   debugger;
-  //   if (
-  //     props.mainTableDataProp !== undefined &&
-  //     router.query.id !== undefined
-  //   ) {
-  //     let pageObject = props.mainTableDataProp.filter(
-  //       row => row.id == router.query.id
-  //     );
-  //     return pageObject[0];
-  //   }
-  // };
-  //console.log(dataObject);
-  //debugger;
-  // const onChangeHandler = (event) =>{
-  //     debugger;
-  //     if (dataObject !== undefined && event !== undefined) {
-  //         event.target.value = dataObject.description
-  //     }
-  // }
-  //console.log((props.mainTableDataProp.filter(row => row.id === router.query.id))[0].description);
-  const getDescription = () => {
+  const getObjectPropValue = objectProperty => {
     if (
       props.mainTableDataProp !== undefined &&
       router.query.id !== undefined
@@ -40,50 +15,95 @@ export function SingleRequest(props) {
       let smthing = props.mainTableDataProp.filter(
         row => row.id == router.query.id
       );
-      return smthing[0].description;
+      return smthing[0][objectProperty];
     }
   };
   return (
     <form>
       <div className="background-headline"> פרטי תיק</div>
-      <div className="label-input-container">
-        <label>תיאור</label>
-        <input
-          className="input-large"
-          type="text"
-          id="description"
-          name="description"
-          value={getDescription()}
-        ></input>
-      </div>
-      <div className="label-input-container">
-        <label>ענף</label>
-        <select className="select" name="cars" id="cars">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="opel">Opel</option>
-          <option value="audi">Audi</option>
-        </select>
-      </div>
-      <div className="label-input-double-container">
-        <label className="label-half">סוג תיק
-        <input
-          className="input-small"
-          type="text"
-          id="description"
-          name="description"
-          value={getDescription()}
-        ></input>
-        </label>
-        <label className="label-half">סיווג
-        <input
-          className="input-small"
-          type="text"
-          id="description"
-          name="description"
-          value={getDescription()}
-        ></input>
-        </label>
+      <div className="form-container">
+        <div className="half-form-container">
+          <div className="label-input-container">
+            <label>תיאור</label>
+            <input
+              className="input-large"
+              type="text"
+              id="description"
+              name="description"
+              value={getObjectPropValue("description")}
+            ></input>
+          </div>
+          <div className="label-input-container">
+            <label>ענף</label>
+            <select name="cars" id="cars">
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="opel">Opel</option>
+              <option value="audi">Audi</option>
+            </select>
+          </div>
+          <div className="label-input-double-container">
+            <label className="label-half">
+              סוג תיק
+              <br></br>
+              <select name="cars" id="cars">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+            </label>
+            <label className="label-half">
+              סיווג
+              <select name="cars" id="cars">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+            </label>
+          </div>
+          <label>גוף דורש</label>
+          <div className="images-conitaner">
+            <img src="/images/mafat.jpg"></img>
+            <img
+              src="/images/Israel-Defense-Forces.svg.png"
+              style={{ height: "100px", marginTop: "20px" }}
+            ></img>
+          </div>
+          <div className="label-input-container">
+            <label>פרויקט</label>
+            <input
+              className="input-large"
+              type="text"
+              id="project"
+              name="project"
+              value={getObjectPropValue("project")}
+            ></input>
+          </div>
+          <div className="label-input-double-container">
+            <input placeholder="אחוז השתתפות ספק"></input>
+          </div>
+          <div className="label-input-double-container">
+            <input
+              className="input-small"
+              type="text"
+              id="project"
+              name="project"
+              value={getObjectPropValue("amount")}
+            ></input>
+            <div
+              className="label-input-double-container"
+              style={{ marginTop: "0px" }}
+            >
+              <div className="currency">ILS</div>
+              <div className="currency">USD</div>
+            </div>
+          </div>
+        </div>
+        <div className="half-form-container"></div>
+      
+      
       </div>
     </form>
   );
@@ -97,7 +117,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onInitMainTable: tags => dispatch(actions.initMainTable(tags))
+    //onInitMainTable: tags => dispatch(actions.initMainTable(tags))
   };
 };
 
